1cd2924de2c0c14435ef5dc0c18f2c6ccd5cf329
Fyre <tanzen6@verizon.net>
2018-12-13 20:05:44 -0500

    Updated for gcloud

--- web/connect.php
+++ web/connect.php
@@ -1,15 +1,16 @@
 <!-- This creates the connection string used by the website to connect to the database -->
 <?php
-$servername = "127.0.0.1";
-$username = "root";
-$password = "";
+//$servername = "/cloudsql/airportdatabase:us-east1:airportdatabase";
+//$username = "root";
+//$password = "";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+//$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli(null, 'root', null, 'airport_database', null, '/cloudsql/aiportdatabase:us-east1:airportdatabase');
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-$conn->select_db("airportdatabase");
+$conn->select_db("airport_database");
 ?>
